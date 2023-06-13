4997c5dcf745ddd6f8569eb0c663894d49098a91
Danny De Novi <70696078+dannydenovi@users.noreply.github.com>
2023-06-13 20:22:25 +0200

    Aggiunta commenti

--- home.py
+++ home.py
@@ -8,38 +8,50 @@ from random import randint
 from paho.mqtt.client import Client as mqtt
 from time import sleep
 
+# Parsing per disabilitare l'utilizzo del sensore di temperatura
+# e abilitare valori randomici
 parser = argparse.ArgumentParser(description='Argument parsing')
 parser.add_argument('-r', '--random', action='store_true', help='Generate random values for temperature.')
 args = parser.parse_args()
 
-
+# CONNESSIONE MQTT
 Broker=mqtt(client_id="raspberry")
 Broker.connect("localhost", 1883)
 
+# DOOR CONFIG
 DOOR_LED = 19
 DOOR_PIN = 18
 
+# MAIN_LIGHT CONFIG
 LIGHT_LED = 26
 LIGHT_PIN = 23
 
+# AIR_CONDITIONER CONFIG
 HOT_AIR_PIN = 24
 COLD_AIR_PIN = 25
 
-
+# IMPOSTA I GPIO IN MODALITÀ BCM
 GPIO.setmode(GPIO.BCM)
+
+# ABILITAZIONE GPIO DOOR
 GPIO.setup(DOOR_LED, GPIO.OUT)
 GPIO.setup(DOOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+# ABILITAZIONE GPIO MAIN_LIGHT
 GPIO.setup(LIGHT_LED, GPIO.OUT)
 GPIO.setup(LIGHT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+# ABILITAZIONE GPIO AIR_CONDITIONER
 GPIO.setup(HOT_AIR_PIN, GPIO.OUT)
 GPIO.setup(COLD_AIR_PIN, GPIO.OUT)
 
+# VARIABILI DI STATO DOOR E MAIN_LIGHT
 door_state = False  
 light_state = False
 
+# CTRL+C HANDLER
 def signal_handler(sig, frame):
+    # PULIZIA DEL BUFFER GPIO
     GPIO.cleanup()
     sys.exit(0)
 
@@ -67,6 +79,7 @@ def measure_temp():
     while True:
         if not args.random:
             try:
+                # MISURA CON SENSORE DI TEMPERATURA
                 _, temperature = Adafruit_DHT.read_retry(11, 4)
                 msg = manage_temp(temperature)
             except:
@@ -108,9 +121,11 @@ def light_button_callback(channel):
     
     Broker.publish(topic="main_light", payload=str(msg))
 
+# AGGIUNTA EVENTI AL CLICK SUI TASTI
 GPIO.add_event_detect(DOOR_PIN, GPIO.FALLING, callback=door_button_callback, bouncetime=200)
 GPIO.add_event_detect(LIGHT_PIN, GPIO.FALLING, callback=light_button_callback, bouncetime=200)
 
+# THREAD MISURA TEMPERATURA
 temp_thread = Thread(target=measure_temp)
 temp_thread.start()
 
