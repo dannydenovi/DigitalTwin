6c0b54925636f19791c214d9b6b2c44853690116
Danny De Novi <70696078+dannydenovi@users.noreply.github.com>
2023-06-13 23:15:19 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import platform
+import subprocess
+
+dependencies = [
+    'paho-mqtt',
+]
+
+# Controlla l'architettura e aggiungi la dipendenza se necessario
+if platform.machine() == 'x86_64':
+    dependencies.append('coppeliasim_zmqremoteapi_client')
+
+# Esegui comandi specifici per Raspberry Pi
+if platform.machine().startswith('arm') and platform.system() == 'Linux':
+    subprocess.run(['sudo', 'apt', 'install', 'mosquitto'])
+    subprocess.run(['sudo', 'systemctl', 'enable', 'mosquitto.service'])
+    subprocess.run(['echo', 'listener 1883 | sudo tee -a /etc/mosquitto/mosquitto.conf'])
+    subprocess.run(['echo', 'allow_anonymous true | sudo tee -a /etc/mosquitto/mosquitto.conf'])
+    subprocess.run(['git', 'clone', 'https://github.com/adafruit/Adafruit_Python_DHT.git'])
+    subprocess.run(['cd', 'Adafruit_Python_DHT', '&&', 'sudo', 'python', 'setup.py', 'install'])
+
+setup(
+    name='nome_del_progetto',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=dependencies,
+)
